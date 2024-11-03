0c844c951b738b582caa29043880881d9ef58d4a
Rabia waheed <Ahtishamwaheed0786@gmail.com>
2024-11-03 16:47:05 +0500

    first commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function Footer(){
     return(
-        <div style={{marginTop:"400px", display:"flex", background:"seagreen", justifyContent:"centre"}}>
+        <div style={{marginTop:"400px", display:"flex", background:"pink", justifyContent:"centre"}}>
             <p style={{marginLeft:"300px", paddingLeft:"250px"}}><b>Created by @Rabia Waheed</b></p>
         </div>
     )
